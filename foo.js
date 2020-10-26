f82a898b4982be0b2795a88218b4972ca0713029
seungtaek95 <seungtaek95@gmail.com>
2020-10-26 22:52:40 +0900

    regex 2

--- foo.js
+++ foo.js
@@ -1,5 +1,6 @@
-const reg = /x(?=y)/
-
-const foo = 'xyxaxb';
-
-console.log(foo.match(reg));
+const myRegex = /(Seoul|Incheon)/;
+const result = "I live in Seoul, I love Incheon".replace(myRegex, "Busan");
+console.log(result);
+const myRegex2 = /(Seoul|Incheon)/g;
+const result2 = "I live in Seoul, I love Incheon".replace(myRegex2, "Busan");
+console.log(result2);
