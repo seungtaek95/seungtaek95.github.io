d9eee3cfdaa3dec7e8a22cd77645d6ef713bf140
seungtaek95 <seungtaek95@gmail.com>
2020-11-23 23:20:34 +0900

    mongoose

--- foo.js
+++ foo.js
@@ -1,6 +1,5 @@
-const myRegex = /(Seoul|Incheon)/;
-const result = "I live in Seoul, I love Incheon".replace(myRegex, "Busan");
-console.log(result);
-const myRegex2 = /(Seoul|Incheon)/g;
-const result2 = "I live in Seoul, I love Incheon".replace(myRegex2, "Busan");
-console.log(result2);
+function foo(age) {
+  return age === 26 ? true : false
+}
+
+console.log(foo(26))
